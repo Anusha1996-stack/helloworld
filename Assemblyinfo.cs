0c9988e0a6b0b90eb76089f446d207e0a687a83e
Anusha1996-stack <f.anusha@globaledgesoft.com>
2021-06-28 16:21:14 +0530

    message

--- Assemblyinfo.cs
+++ Assemblyinfo.cs
@@ -1,8 +1,8 @@
-Assembly Version :1.0.1.1
+Assembly Version :1.0.0.4
 Assembly Title : Test_Product
 Assembly Description : Test_Job
 Assembly Company : Test_Company
-Assembly Product :1.0.1.1
+Assembly Product :1.0.0.4
 Assembly Copyright : Test_Copyright
 Assembly Trademark : Test_Trademark
 Assembly Culture : Test_Culture
